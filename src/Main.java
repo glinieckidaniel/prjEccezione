67c3f65e98ab4b6aa486b3919c7c843b7728c635
glinieckidaniel <daniel.gliniecki@buonarorti.tn.it>
2025-11-17 10:10:21 +0100

    eccezioni

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,13 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-        int d = 5;
-        int s = 0;
-
-        try {
-            int h = d / s;
-        } catch (Exception e) {
-            System.out.println("Eccezione gestita");
-        }
+        Scanner s1 = new Scanner(s1);
+        System.out.println("Inserisci una stringa di 20 caratteri : ");
+        System.out.println(" " + s1.nextLine());
+        s1.close();
     }
 
 }
---
+++ src/MessaggioTroppoLungoException.java
@@ -0,0 +1,12 @@
+public class MessaggioTroppoLungoException extends Exception {
+
+
+    public MessaggioTroppoLungoException(String messaggio) {
+        super(messaggio);
+    }
+
+    public MessaggioTroppoLungoException(){
+        super("Messaggio troppo lungo");
+    }
+
+}
