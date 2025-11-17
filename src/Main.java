06076e82f91e5afc6d0c61a55fa8141500a26c0a
glinieckidaniel <daniel.gliniecki@buonarorti.tn.it>
2025-11-17 10:12:39 +0100

    eccezioni

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,12 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Scanner s1 = new Scanner(s1);
+        Scanner s1 = new Scanner(System.in);
         System.out.println("Inserisci una stringa di 20 caratteri : ");
-        System.out.println(" " + s1.nextLine());
+        s1.nextLine();
         s1.close();
+
+
     }
 
 }
