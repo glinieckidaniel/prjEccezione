ea46d1c892110216f0f8f5981327db816fb1e75c
glinieckidaniel <daniel.gliniecki@buonarorti.tn.it>
2025-11-17 10:38:15 +0100

    eccezioni

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,11 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        Scanner s1 = new Scanner(System.in);
-        System.out.println("Inserisci una stringa di 20 caratteri : ");
-        s1.nextLine();
-        s1.close();
+    public static void main(String[] args) throws MessaggioTroppoLungoException {
 
+        try{
+            System.out.println(frase());
+        }catch()
 
     }
 
